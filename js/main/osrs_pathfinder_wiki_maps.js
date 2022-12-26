6bc57b3b20fc0336d3d3d95866d1739613be4041
Oliver Zeidler <oliverzeidler97@gmail.com>
2022-12-26 19:16:16 +0100

    small code restructuring

--- js/main/osrs_pathfinder_wiki_maps.js
+++ js/main/osrs_pathfinder_wiki_maps.js
@@ -1,24 +1,6 @@
 // Path from 3221,3220,0 to 3226,3220,0
 const initPath = JSON.parse(`[{"destination":{"x":3221,"y":3220,"z":0},"methodOfMovement":"start"},{"destination":{"x":3222,"y":3221,"z":0},"methodOfMovement":"walk north east"},{"destination":{"x":3223,"y":3221,"z":0},"methodOfMovement":"walk east"},{"destination":{"x":3224,"y":3221,"z":0},"methodOfMovement":"walk east"},{"destination":{"x":3225,"y":3220,"z":0},"methodOfMovement":"walk south east"},{"destination":{"x":3226,"y":3220,"z":0},"methodOfMovement":"walk east"}]`);
-
-
 const API_URL = "http://localhost:8100"
-
-async function findPath(start, end) {
-    const response = await fetch(`${API_URL}/path.json` +
-        `?from=${start.x},${start.y},${start.z}` +
-        `&to=${end.x},${end.y},${end.z}`);
-
-    const responseBody = await response.json();
-    console.log(responseBody);
-    if(responseBody.pathFound) {
-        drawPath(responseBody.path);
-    }
-}
-
-function Coordinate(x, y, z) {
-    return { x, y, z };
-}
 
 
 let startMarker = L.marker([3220.5, 3221.5], {
@@ -35,7 +17,6 @@ let endMarker = L.marker([3220.5, 3226.5], {
     alt: 'end point marker',
     draggable: true
 });
-
 endMarker.on('dragend', markerMoved);
 endMarker.addTo(runescape_map);
 
@@ -43,6 +24,22 @@ let currentlyDrawnPath = null;
 drawPath(initPath);
 
 
+
+async function findPath(start, end) {
+    const response = await fetch(`${API_URL}/path.json` +
+        `?from=${start.x},${start.y},${start.z}` +
+        `&to=${end.x},${end.y},${end.z}`);
+
+    const responseBody = await response.json();
+    console.log(responseBody);
+    if(responseBody.pathFound) {
+        drawPath(responseBody.path);
+    }
+}
+
+function Coordinate(x, y, z) {
+    return { x, y, z };
+}
 
 function markerMoved() {
     const startCoordinate = getCoordinatesFromMarker(startMarker);
