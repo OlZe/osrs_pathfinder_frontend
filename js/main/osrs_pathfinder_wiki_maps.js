54877fb89113c2dc896fac7f4e8fe34d37ac634a
Oliver Zeidler <oliverzeidler97@gmail.com>
2022-12-28 19:16:53 +0100

    minimized use of global variables

--- js/main/osrs_pathfinder_wiki_maps.js
+++ js/main/osrs_pathfinder_wiki_maps.js
@@ -1,6 +1,6 @@
 /**
  * @typedef {Object} PathResultMovement
- * @property {String} methodOfMovement
+ * @property {string} methodOfMovement
  * @property {Coordinate} destination
  */
 
@@ -11,28 +11,6 @@
  * @property {PathResultMovement[]} path
  */
 
-
-class PathFetcher {
-    static #API_URL = "http://localhost:8100"
-
-    /**
-     * @param {Coordinate} start
-     * @param {Coordinate} end
-     * @returns {Promise<PathResult>} path
-     */
-    async fetchPath(start, end) {
-        const response = await fetch(`${PathFetcher.#API_URL}/path.json`, {
-            method: 'POST',
-            body: JSON.stringify({
-                from: start,
-                to: end,
-                blacklist: window.PathFinder._blacklist
-            })
-        });
-        return response.json();
-    }
-}
-
 class Coordinate {
     constructor(x, y, z) {
         this.x = x;
@@ -42,9 +20,30 @@ class Coordinate {
 }
 
 
+class PathFetcher {
+    static #API_URL = "http://localhost:8100"
+
+    /**
+     * @param {Coordinate} start
+     * @param {Coordinate} end
+     * @param {string[]} blacklist
+     * @returns {Promise<PathResult>} path
+     */
+    async fetchPath(start, end, blacklist) {
+        const response = await fetch(`${PathFetcher.#API_URL}/path.json`, {
+            method: 'POST',
+            body: JSON.stringify({
+                from: start,
+                to: end,
+                blacklist: blacklist
+            })
+        });
+        return response.json();
+    }
+}
+
+
 class MapInteractor {
-    // Path from 3221,3220,0 to 3226,3220,0
-    static #INIT_PATH = JSON.parse(`[{"destination":{"x":3221,"y":3220,"z":0},"methodOfMovement":"start"},{"destination":{"x":3222,"y":3221,"z":0},"methodOfMovement":"walk north east"},{"destination":{"x":3223,"y":3221,"z":0},"methodOfMovement":"walk east"},{"destination":{"x":3224,"y":3221,"z":0},"methodOfMovement":"walk east"},{"destination":{"x":3225,"y":3220,"z":0},"methodOfMovement":"walk south east"},{"destination":{"x":3226,"y":3220,"z":0},"methodOfMovement":"walk east"}]`);
 
     /**
      * @param {PathFetcher} pathFetcher 
@@ -58,16 +57,28 @@ class MapInteractor {
         this.currentlyOpenPopups = [];
         this.startMarker = null;
         this.endMarker = null;
-        this.init();
+        /**@type {string[]} */
+        this.blacklist = [];
+
+        // Add this object to global scope so the popup blacklist button can call this object's function
+        window.PathMapInteractor = this;
+
+        this.initMapObjects();
+    }
+
+    /**
+     * @param {string} methodOfMovement 
+     */
+    addBlacklistItem(methodOfMovement) {
+        this.blacklist.push(methodOfMovement);
     }
 
     async onMarkerMoved() {
         const startCoordinate = this.getCoordinatesFromMarker(this.startMarker);
         const endCoordinate = this.getCoordinatesFromMarker(this.endMarker);
         console.log('Start: ', startCoordinate, 'End: ', endCoordinate);
-        const pathResult = await this.pathFetcher.fetchPath(startCoordinate, endCoordinate);
-        if(pathResult.pathFound)
-        {
+        const pathResult = await this.pathFetcher.fetchPath(startCoordinate, endCoordinate, this.blacklist);
+        if (pathResult.pathFound) {
             this.drawPath(pathResult.path)
         }
     }
@@ -81,42 +92,42 @@ class MapInteractor {
         }
         const pathCoords = path.map(movement => [movement.destination.y + 0.5, movement.destination.x + 0.5]);
         this.currentlyDrawnPath = L.polyline(pathCoords);
-        this.currentlyDrawnPath.addTo(runescape_map);
+        this.currentlyDrawnPath.addTo(this.map);
         this.drawHelperPopupsForPath(path);
     }
-    
+
     /**
      * @param {PathResultMovement[]} path 
      */
     drawHelperPopupsForPath(path) {
         // Close popups
         this.currentlyOpenPopups.forEach(p => p.remove());
-    
+
         // Draw popups
         path.forEach((movement, index) => {
             // Don't draw popup "to" starting tile or on walking paths
             if (index == 0 || movement.methodOfMovement.includes('walk')) {
                 return;
             }
-    
+
             const destinationCoordinates = movement.destination;
             const sourceCoordinates = path[index - 1].destination;
-    
+
             // If the popup is going from the starting tile, it could hide the start marker, move it up a little
             if (index == 1) {
                 sourceCoordinates.y += 1;
             }
-    
+
             const popupOptions = { closeButton: false, closeOnEscapeKey: false, autoClose: false, closeOnClick: false, maxWidth: 150, autoPan: false };
             // Let popup show the teleport name, on click pan to destination
-            const popupContent = 
+            const popupContent =
                 `<div 
                     onclick="runescape_map.panTo([${destinationCoordinates.y + 0.5},${destinationCoordinates.x + 0.5}], { animate: true })"
                     style="cursor: alias">
                         ${movement.methodOfMovement}
                         <button
                             title="Blacklist this teleport/transport"
-                            onclick="window.PathFinder.addToBlacklist('${movement.methodOfMovement}')">
+                            onclick="window.PathMapInteractor.addBlacklistItem('${movement.methodOfMovement}')">
                             &#9940;
                         </button>
                 </div>`;
@@ -127,7 +138,7 @@ class MapInteractor {
             this.currentlyOpenPopups.push(popup);
         })
     }
-    
+
     /**
      * Extracts integer coordinates out of a marker
      * @param {*} marker The Leaflet marker object
@@ -141,8 +152,11 @@ class MapInteractor {
             0
         );
     }
-        
-    init() {
+
+    initMapObjects() {
+        // Path from 3221,3220,0 to 3226,3220,0
+        const INIT_PATH = JSON.parse(`[{"destination":{"x":3221,"y":3220,"z":0},"methodOfMovement":"start"},{"destination":{"x":3222,"y":3221,"z":0},"methodOfMovement":"walk north east"},{"destination":{"x":3223,"y":3221,"z":0},"methodOfMovement":"walk east"},{"destination":{"x":3224,"y":3221,"z":0},"methodOfMovement":"walk east"},{"destination":{"x":3225,"y":3220,"z":0},"methodOfMovement":"walk south east"},{"destination":{"x":3226,"y":3220,"z":0},"methodOfMovement":"walk east"}]`);
+
         this.startMarker = L.marker([3220.5, 3221.5], {
             title: 'start point',
             alt: 'start point marker',
@@ -157,23 +171,10 @@ class MapInteractor {
         });
         this.endMarker.on('dragend', this.onMarkerMoved.bind(this));
         this.endMarker.addTo(this.map);
-        this.drawPath(MapInteractor.#INIT_PATH);
+        this.drawPath(INIT_PATH);
     }
-    
+
 }
-
-
-
 
 const pathFetcher = new PathFetcher();
 const mapInteractor = new MapInteractor(pathFetcher, runescape_map)
-
-
-
-window.PathFinder = {
-    _blacklist: [],
-    addToBlacklist: function(methodOfMovement) {
-        window.PathFinder._blacklist.push(methodOfMovement);
-        console.log("New Blacklist Entry:", methodOfMovement);
-    }
-}
