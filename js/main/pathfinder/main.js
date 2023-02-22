56342fe635ab156746ea1255503588da0e2ed8b6
Oliver Zeidler <oliverzeidler97@gmail.com>
2023-02-22 18:40:31 +0100

    Updated map

--- js/main/pathfinder/main.js
+++ js/main/pathfinder/main.js
@@ -1,7 +1,7 @@
 import BlacklistManager from "./blacklist-manager.js";
 import DataFetcher from "./data-fetcher.js";
 import MapInteractor from "./map-interactor.js";
-
+import Coordinate from "./coordinate.js";
 
 if (!L || !runescape_map) {
     throw ("Global variables L or runescape_map are missing");
@@ -15,3 +15,18 @@ await blacklistManager.initBlacklist();
 
 const mapInteractor = new MapInteractor(dataFetcher, blacklistManager, runescape_map);
 mapInteractor.initMapObjects();
+
+
+window.debug = {
+    dataFetcher,
+    blacklistManager,
+    mapInteractor,
+    Coordinate,
+    getAndDrawPath: async (start, end) => {
+        const result = await dataFetcher.fetchPath(start, end, blacklistManager.getBlacklist());
+        if(result.pathFound) {
+            mapInteractor._drawPath(result.path);
+        }
+        console.log(result);
+    }
+};
